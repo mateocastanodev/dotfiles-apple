47c1e00cc2da0abf3079c1bebade05e5fdff96c1
Wout Vossen <wout@L51WQLR.cmc.be>
2026-06-12 09:18:54 +0200

    New debugpy commands

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -220,6 +220,40 @@ dap.configurations.python = { -- https://github.com/microsoft/debugpy/wiki/Debug
 		request = 'launch',
 		name = 'Launch file',
 		program = '${file}',
+		justMyCode = false,
+		python = function()
+			local root = vim.fs.root(0, '.venv')
+			return { root and root .. '/.venv/bin/python' or 'python3' }
+		end,
+		cwd = function()
+			return vim.fs.root(0, '.venv') or vim.fn.getcwd()
+		end,
+	},
+	{
+		type = 'debugpy',
+		request = 'launch',
+		name = 'Pytest current file',
+		module = 'pytest',
+		args = { '${file}', '-s' },
+		justMyCode = false,
+		python = function()
+			local root = vim.fs.root(0, '.venv')
+			return { root and root .. '/.venv/bin/python' or 'python3' }
+		end,
+		cwd = function()
+			return vim.fs.root(0, '.venv') or vim.fn.getcwd()
+		end,
+	},
+	{
+		type = 'debugpy',
+		request = 'launch',
+		name = 'Pytest current file -k',
+		module = 'pytest',
+		args = function()
+			local test_name = vim.fn.input('pytest -k: ')
+			return { '${file}', '-s', '-k', test_name }
+		end,
+		justMyCode = false,
 		python = function()
 			local root = vim.fs.root(0, '.venv')
 			return { root and root .. '/.venv/bin/python' or 'python3' }
